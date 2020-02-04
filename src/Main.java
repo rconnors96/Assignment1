48d34b4ae7b6ccabf9e9b10ca9ce037ecc152419
Ryan <r3connors@student.bridgew.edu>
2020-02-04 17:11:06 -0500

    Currently adds all txt files to a File array, and is capable of printing the entirety of each file by calling printFiles() with the File array obtained using getFiles()

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import jdk.jfr.StackTrace;
+
+import java.io.*;
+import java.nio.file.*;
+
+public class Main {
+
+    public static void main(String []args) {
+        File[] files = getFiles();
+        printFiles(files);
+
+
+    }
+
+    private static File[] getFiles(){
+        Path folderPath = FileSystems.getDefault().getPath("txtFolder");
+        File txtFolder = folderPath.toFile();
+        return txtFolder.listFiles();
+    }
+
+    private static void printFiles(File[] files) {
+        String line;
+        for (File current : files) {
+            try (BufferedReader out = new BufferedReader(new FileReader(current))) {
+                while((line = out.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch(IOException ex) {
+                System.err.println(ex);
+            }
+        }
+    }
+}
