8d4167955d8a6c405cc6ffb94ec3aba59a1a12ef
Ryan <r3connors@student.bridgew.edu>
2020-02-04 17:42:23 -0500

    progress on wordCount method

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,12 @@
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.Hashtable;
 
 public class Main {
+
+    //Hashtable can be shared between multiple threads, HashMap can not.
+    private static final Hashtable wordCount = new Hashtable();
 
     public static void main(String []args) {
         File[] files = getFiles();
@@ -28,6 +32,28 @@ public class Main {
             try (BufferedReader out = new BufferedReader(new FileReader(current))) {
                 while((line = out.readLine()) != null) {
                     System.out.println(line);
+                }
+            } catch(IOException ex) {
+                System.err.println(ex);
+            }
+        }
+    }
+
+    private static void countWords(File[] files) {
+        String line;
+        char curChar;
+
+
+        for (File current : files) { //goes through each txt file in the directory
+            try (BufferedReader out = new BufferedReader(new FileReader(current))) { //tries to create a BufferedReader object with the current file
+                while((line = out.readLine()) != null) { //goes until there are no more lines in the file
+                    String word = "";
+                    for (int i = 0; i < line.length(); i++) { //goes until there are no more characters in the line
+                        curChar = line.charAt(i); //sets the current character in the line to curChar
+                        if (curChar != ' ') {
+                            word += curChar; //START HERE, TRY STRINGBUILDER
+                        }
+                    }
                 }
             } catch(IOException ex) {
                 System.err.println(ex);
